3b066a23e76059026b95a30a6e7712de9f5ab12f
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-10 16:23:56 +0100

    end of second bonus

--- bonus_2/main.js
+++ bonus_2/main.js
@@ -1,2 +1,5 @@
-
-
+let now = new Date();
+const minuts = now.getMinutes()
+const maxMin = 60;
+const missingMinuites = maxMin - minuts;
+alert(`"mancano esattamente "${missingMinuites} "min allo scoccare dell'ora`)
