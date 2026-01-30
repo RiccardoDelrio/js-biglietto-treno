34b0cff29cd4bf6da0aed269d5970c27da343ae0
Riccardo <riccardodelrio92@gmail.com>
2026-01-30 11:06:55 +0100

    Update project structure

--- main.js
+++ main.js
@@ -3,29 +3,29 @@ const kmDaPercorrere = Number(prompt("Inserire il numero di km che si vuole perc
 console.log(kmDaPercorrere);
 
 /* Richiesta dell'età dell'utente*/
-const eta = Number(prompt("Inserisci la tua età"))
+const eta = Number(prompt("Inserisci la tua età"));
 console.log(eta);
 
 /* ricavare costo del biglietto senza sconti */
 
-const prezzoLordo = kmDaPercorrere * 0.21
+const prezzoLordo = kmDaPercorrere * 0.21;
 console.log(prezzoLordo);
 
 if (eta < 18) {
     let sconto = (prezzoLordo * 20) / 100;
-    console.log(sconto);
+    // console.log(sconto);
     const prezzoNetto = (prezzoLordo - sconto);
-    console.log(prezzoNetto.toFixed(2));
-    alert(`${"Il prezzo da pagare è"}, ${prezzoNetto.toFixed(2)}`)
-} else if (eta > 65) {
+    // console.log(prezzoNetto.toFixed(2));
+    alert(`Il prezzo da pagare è ${prezzoNetto.toFixed(2)}€`);
+} else if (eta >= 65) {
     sconto = (prezzoLordo * 40) / 100;
-    console.log(sconto);
+    // console.log(sconto);
     const prezzoNetto = (prezzoLordo - sconto);
-    console.log(prezzoNetto.toFixed(2));
-    alert(`${"Il prezzo da pagare è"}, ${prezzoNetto.toFixed(2)}`)
+    // console.log(prezzoNetto.toFixed(2));
+    alert(`Il prezzo da pagare è ${prezzoNetto.toFixed(2)}€`);
 } else {
-    console.log(prezzoLordo.toFixed(2))
-    alert(`${"Il prezzo da pagare è"}, ${prezzoLordo.toFixed(2)}`);
+    // console.log(prezzoLordo.toFixed(2));
+    alert(`Il prezzo da pagare è ${prezzoLordo.toFixed(2)}€`);
 
 }
 
